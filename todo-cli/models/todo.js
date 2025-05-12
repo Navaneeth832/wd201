271b28a6f3cbc3587462701d6cf9755f4412b20e
Navaneeth832 <mittunavan@gmail.com>
2025-05-12 06:44:40 +0000

    orrapp

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -68,16 +68,13 @@ module.exports = (sequelize, DataTypes) => {
 
     displayableString() {
       const checkbox = this.completed ? "[x]" : "[ ]";
-      const today = new Date().toISOString().split("T")[0];
-    
-      let dueDateDisplay = "";
-    
-      if (this.dueDate !== today) {
-        dueDateDisplay = this.dueDate;
-      }
-    
-      return `${this.id}. ${checkbox} ${this.title.trim()}${dueDateDisplay ? " " + dueDateDisplay : ""}`;
+      const dueDateDisplay =
+        this.dueDate === new Date().toISOString().split("T")[0]
+          ? ""
+          : ` ${this.dueDate}`; 
+      return `${this.id}. ${checkbox} ${this.title.trim()}${dueDateDisplay}`;
     }
+    
     
   }
 
